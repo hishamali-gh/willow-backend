6c0ac53e378cdbd3c058d3d4cf1e7dcf55ae3689
hisham Ali <thehishamali@gmail.com>
2026-02-20 09:37:45 +0530

    Tenth Commit

--- wishlist/serializers.py
+++ wishlist/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
+from products.serializers import ProductModelSerializer
 from .models import Wishlist
+from products.models import Product
 
 class WishlistModelSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        write_only=True
+    )
+    product_details = ProductModelSerializer(
+        source='product',
+        read_only=True
+    )
+
     class Meta:
         model = Wishlist
-        fields = ['id', 'product', 'created_at']
+        fields = ['id', 'product', 'product_details', 'created_at']
         read_only_fields = ['id', 'created_at']
+
